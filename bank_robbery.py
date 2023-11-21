e5273a12901490b9db4da019859e77d727487378
johnbagshaw007 <151597980+johnbagshaw007@users.noreply.github.com>
2023-11-20 22:47:46 -0500

    Create bank_robbery.py
    Fundamental python exercise for a bank robbery incident

---
+++ bank_robbery.py
@@ -0,0 +1,12 @@
+# '''Bank Robbery Money
+# I just robbed a bank with some of my friends.  
+# We got away with $19,867,324,678,987.99, and now we have to split it up 5 ways.  
+# Print out the dollar amount each robber gets to keep.
+
+# p.s. I haven't actually robbed a bank.  Yet.'''
+
+cash = 19867324678987.99 # fixed in this instance, can be user-specific
+cash_split_in_5 = cash/5
+print(cash_split_in_5)
+
+#print(cash/5)
